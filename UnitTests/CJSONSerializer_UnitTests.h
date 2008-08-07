226e8a98cebd38b237944100737d96250aa641d3
jwight <jwight@43e68342-e247-0410-8716-5592636d597e>
2008-08-07 17:26:20 +0000

    Added in Luis' patch for Serializer/Deserializer unit tests. #25
    git-svn-id: http://touchcode.googlecode.com/svn/trunk@102 43e68342-e247-0410-8716-5592636d597e
    
    committer: jwight <jwight@43e68342-e247-0410-8716-5592636d597e>

--- UnitTests/CJSONSerializer_UnitTests.h
+++ UnitTests/CJSONSerializer_UnitTests.h
@@ -3,7 +3,7 @@
 //  TouchJSON
 //
 //  Created by Jonathan Wight on 12/12/2005.
-//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//  Copyright 2005 Toxic Software. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- UnitTests/CJSONSerializer_UnitTests.m
+++ UnitTests/CJSONSerializer_UnitTests.m
@@ -3,7 +3,7 @@
 //  TouchJSON
 //
 //  Created by Jonathan Wight on 12/12/2005.
-//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//  Copyright 2005 Toxic Software. All rights reserved.
 //
 
 #import "CJSONSerializer_UnitTests.h"
@@ -11,5 +11,17 @@
 #import "CJSONSerializer.h"
 
 @implementation CJSONSerializer_UnitTests
+-(void)testEmptyDictionary {
+	NSString *jsonEquivalent = @"{}";
+	NSDictionary *emptyDictionary = [NSDictionary dictionary];
+	id theObject = [[CJSONSerializer serializer] serializeObject:emptyDictionary];
+	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+}
 
+-(void)testSingleKeyValuePair {
+	NSString *jsonEquivalent = @"{\"a\":\"b\"}";
+	NSDictionary *dictionary = [NSDictionary dictionaryWithObject:@"b" forKey:@"a"];
+	id theObject = [[CJSONSerializer serializer] serializeObject:dictionary];
+	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+}
 @end
